aa03a896ca0dee957e3938dc41b7291d9d73eb46
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-25 12:46:36 -0500

    Added hashing on user password and user registration

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected')
+    })
+    .catch(err => {
+        console.log('Oh no, error')
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('this is the home page')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {
