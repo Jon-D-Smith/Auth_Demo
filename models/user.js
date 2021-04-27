2b4120057dc3adcd9b023cf0b40082c2e2069eca
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-26 19:51:08 -0500

    Added middleware for hashing passwords

--- models/user.js
+++ models/user.js
@@ -20,4 +20,13 @@ userSchema.statics.findAndValidate = async function (username, password) {
     return isValid ? foundUser : false;
 }
 
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
+
 module.exports = mongoose.model('User', userSchema);
